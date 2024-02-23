23f97b9f2733b54d1227672f751af32a6a564dcb
Debabrata Acharya <11374405+onclave@users.noreply.github.com>
2024-02-23 21:07:58 +0530

    feat: add BmxApplication annotation

--- src/main/java/com/bloomscorp/bsb/BmxApplication.java
+++ src/main/java/com/bloomscorp/bsb/BmxApplication.java
@@ -1,15 +1,20 @@
 package com.bloomscorp.bsb;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-public class BmxApplication implements ErrorController {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
-	@RequestMapping(value = "/error")
-	@ResponseStatus(HttpStatus.OK)
-	public String error() {
-		return "forward:/index.html";
-	}
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Controller
+@SpringBootApplication
+public @interface BmxApplication {
 }
